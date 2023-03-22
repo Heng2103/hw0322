78cd675a5ea5fcbcd7eb38d6ba93e0fa72aadde3
Nigel <o1103028@gm.pu.edu.tw>
2023-03-22 14:00:13 +0800

    v.1(许梓恒)

--- app/src/main/java/tw/edu/pu/o1103028/hw03222/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1103028/hw03222/MainActivity.kt
@@ -2,10 +2,18 @@ package tw.edu.pu.o1103028.hw03222
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txv: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener({
+            txv.text = "许梓恒!"
+        })
     }
 }
